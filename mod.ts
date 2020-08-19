e53d0dafd4cdcf3cf0a289d85db3f7d3f97d0dcb
justjavac <justjavac@gmail.com>
2020-08-19 23:12:09 +0800

    release v0.1.0

--- mod.ts
+++ mod.ts
@@ -1,15 +1,29 @@
-/**
- * Every exported symbol ideally should have a documentation line.
+/** Returns the path to the user's public directory.
  *
- * It is important that documentation is easily human readable,
- * but there is also a need to provide additional styling information to ensure
- * generated documentation is more rich text.
- * Therefore JSDoc should generally follow markdown markup to enrich the text.
- *
- * follow https://deno.land/std/style_guide.md
- *
- * @param foo - Description of non obvious parameter
+ * The returned value depends on the operating system and is either a string,
+ * containing a value from the following table, or `null`.
+ * 
+ * |Platform | Value                 | Example                 |
+ * | ------- | --------------------- | ----------------------- |
+ * | Linux   | `XDG_PUBLICSHARE_DIR` | /home/justjavac/Public  |
+ * | macOS   | `$HOME`/Public        | /Users/justjavac/Public |
+ * | Windows | `{FOLDERID_Public}`   | C:\Users\Public         |
  */
-export default function starter(foo: string): string {
-  return foo;
+export default function publicDir(): string | null {
+  switch (Deno.build.os) {
+    case "linux": {
+      return Deno.env.get("XDG_PUBLICSHARE_DIR") ?? null;
+    }
+
+    case "darwin": {
+      const home = Deno.env.get("HOME");
+      if (home) return `${home}/Public`;
+      break;
+    }
+
+    case "windows":
+      return Deno.env.get("FOLDERID_Public") ?? null;
+  }
+
+  return null;
 }
